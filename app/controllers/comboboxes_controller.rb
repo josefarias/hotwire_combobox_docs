807e7040008f2dd79082e3a3e69b4fd7cfc232dc
Jose Farias <jose@37signals.com>
2024-02-28 21:58:21 -0600

    Bump cache

--- app/controllers/comboboxes_controller.rb
+++ app/controllers/comboboxes_controller.rb
@@ -1,6 +1,6 @@
 class ComboboxesController < ApplicationController
   def index
     @user = User.first
-    fresh_when last_modified: DateTime.parse("2024-02-27 00:00:00")
+    fresh_when last_modified: DateTime.parse("2024-02-28 00:00:00")
   end
 end
